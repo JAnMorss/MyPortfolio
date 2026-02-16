be793a39ceabaed97e1c12c0939bc3f08e03086b
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-16 15:58:04 +0800

    Add Skill ValueObjects

---
+++ src/MyPortfolio.Domain/Skills/ValueObjects/SkillName.cs
@@ -0,0 +1,40 @@
+﻿using MyPortfolio.Domain.Projects.ValueObjects;
+using MyPortfolio.SharedKernel.Domain;
+using MyPortfolio.SharedKernel.ErrorHandling;
+
+namespace MyPortfolio.Domain.Skills.ValueObjects;
+
+public sealed class SkillName : ValueObject
+{
+    public string Value { get; }
+    public const int MaxLength = 50;
+
+    public SkillName(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<SkillName> Create(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            return Result.Failure<SkillName>(new Error(
+                "SkillName.Empty",
+                "SkillName cannot be empty."));
+        }
+
+        if (title.Length > MaxLength)
+        {
+            return Result.Failure<SkillName>(new Error(
+                "SkillName.TooLong",
+                $"SkillName is too long. Maximum length is {MaxLength} characters."));
+        }
+
+        return new SkillName(title);
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
