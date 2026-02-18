93ee5808755cc0a5981e0ee0a6fc5e2de63519f1
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-18 20:06:24 +0800

    Add Application abstractions

---
+++ src/MyPortfolio.Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using MyPortfolio.SharedKernel.ErrorHandling;
+using Serilog.Context;
+
+namespace MyPortfolio.Application.Abstractions.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IBaseRequest
+    where TResponse : Result
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        var name = request.GetType().Name;
+
+        try
+        {
+            _logger.LogInformation("Executing request {Request}", name);
+
+            var result = await next();
+
+            if (result.IsSuccess)
+            {
+                _logger.LogInformation("Request {Request} processed successfully", name);
+            }
+            else
+            {
+                using(LogContext.PushProperty("Errors", result.Error, true))
+                {
+                    _logger.LogError("Request {Request} processed with errors", name);
+                }
+            }
+
+            return result;
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogInformation(ex, "Request {Request} proccessing failed", name);
+            throw;
+        }
+    }
+}
---
+++ src/MyPortfolio.Application/Abstractions/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using MyPortfolio.SharedKernel.Exceptions;
+using MyPortfolio.SharedKernel.Mediators.Command;
+
+namespace MyPortfolio.Application.Abstractions.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+     where TRequest : IBaseCommand
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+    private readonly ILogger<ValidationBehavior<TRequest, TResponse>> _logger;
+
+    public ValidationBehavior(
+        IEnumerable<IValidator<TRequest>> validators, 
+        ILogger<ValidationBehavior<TRequest, TResponse>> logger)
+    {
+        _validators = validators;
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request, 
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationErrors = _validators
+            .Select(v => v.Validate(context))
+            .Where(result => result.Errors.Any())
+            .SelectMany(result => result.Errors)
+            .Select(validationFailure => new ValidationError(
+                validationFailure.PropertyName,
+                validationFailure.ErrorMessage
+            ))
+            .ToList();
+
+        if(validationErrors.Any())
+        {
+            _logger.LogError("Validation triggered for {Request}", typeof(TRequest).Name);
+
+            throw new SharedKernel.Exceptions.ValidationException(validationErrors);
+        }
+
+        return await next();
+    }
+}
