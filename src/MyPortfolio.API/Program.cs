136a3b2c03118b18fa3ebf2411a44c0e207fb0cb
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-18 20:05:06 +0800

    Update API startup configuration

--- src/MyPortfolio.API/Program.cs
+++ src/MyPortfolio.API/Program.cs
@@ -1,20 +1,43 @@
+using MyPortfolio.API.Extensions;
+using MyPortfolio.Application;
+using MyPortfolio.Infrastructure;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog((context, loggerConfig) =>
+{
+    loggerConfig.ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
+
+app.UseRequestContextLogging();
+
+app.UseCustomExceptionHandler();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
