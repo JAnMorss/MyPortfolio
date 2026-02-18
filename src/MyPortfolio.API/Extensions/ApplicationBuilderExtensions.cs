e1ad2046765911577d2a5e906e915e2ac311c104
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-18 20:06:14 +0800

    Add API extensions

---
+++ src/MyPortfolio.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.API.Middleware;
+using MyPortfolio.Infrastructure;
+
+namespace MyPortfolio.API.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+
+    public static IApplicationBuilder UseRequestContextLogging(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<RequestContextLoggingMiddleware>();
+
+        return app;
+    }
+}
