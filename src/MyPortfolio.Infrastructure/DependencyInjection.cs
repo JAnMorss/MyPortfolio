466d41ff9ab8abb7547535b225744d0c26b39fb0
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-18 20:06:39 +0800

    Add Infrastructure dependency injection

---
+++ src/MyPortfolio.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyPortfolio.SharedKernel;
+
+namespace MyPortfolio.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        AddPersistence(services, configuration);
+
+        return services;
+    }
+
+    public static void AddPersistence(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+    }
+}
