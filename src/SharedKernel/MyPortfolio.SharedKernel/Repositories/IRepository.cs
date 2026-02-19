800c7e9700e475bfa6a22d2ba30d27bf8de0d804
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-19 20:03:46 +0800

    Add base repository abstractions

---
+++ src/SharedKernel/MyPortfolio.SharedKernel/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyPortfolio.SharedKernel.Domain;
+using MyPortfolio.SharedKernel.Helpers;
+
+namespace MyPortfolio.SharedKernel.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<IEnumerable<T>> GetAllASync(QueryObject query, CancellationToken cancellationToken = default);
+
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    
+    Task<int> CountAsync(Guid userId, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<T>> SearchAsync(SearchQueryObject searchQuery, Guid? userId, CancellationToken cancellationToken = default);
+}
