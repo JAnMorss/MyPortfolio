27abb43bd5ea2d428b16e34e2b65cb3e9f15150b
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-19 20:03:28 +0800

    Add JWT authentication implementation

---
+++ src/MyPortfolio.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MyPortfolio.Application.Abstractions;
+using MyPortfolio.Domain.Users.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyPortfolio.Infrastructure.Authentication;
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtProvider(IOptions<JwtOptions> options)
+    {
+        _jwtOptions = options.Value;
+    }
+
+    public string Generate(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email.Value),
+            new Claim(ClaimTypes.Role, user.Roles.FirstOrDefault()?.Name ?? "Admin")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+
+        using var rng = RandomNumberGenerator.Create();
+
+        rng.GetBytes(randomNumber);
+
+        return Convert.ToBase64String(randomNumber);
+    }
+}
