b54fabe21da880b0ddb452948a1caa5fe6cdb3c5
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-19 20:03:24 +0800

    Add User domain interfaces

---
+++ src/MyPortfolio.Domain/Users/Interface/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyPortfolio.Domain.Common.ValueObjects;
+using MyPortfolio.Domain.Users.Entities;
+using MyPortfolio.SharedKernel.Repositories;
+
+namespace MyPortfolio.Domain.Users.Interface;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<User?> GetByEmailAsync(EmailAddress email, CancellationToken cancellationToken = default);
+}
