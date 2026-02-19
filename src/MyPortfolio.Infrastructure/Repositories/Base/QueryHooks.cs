7c14f07d56ab8fe853c9ff9a47fa45e1af6af3d4
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-19 20:03:32 +0800

    Add repository implementations

---
+++ src/MyPortfolio.Infrastructure/Repositories/Base/QueryHooks.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.SharedKernel.Domain;
+using MyPortfolio.SharedKernel.Helpers;
+
+namespace MyPortfolio.Infrastructure.Repositories.Base;
+
+public abstract class QueryHooks<T> where T : BaseEntity
+{
+    public virtual IQueryable<T> BuildQuery(
+        ApplicationDbContext context,
+        QueryObject query)
+    {
+        return context.Set<T>().AsQueryable();
+    }
+
+    public virtual IQueryable<T> ApplyFilters(
+        IQueryable<T> query,
+        SearchQueryObject searchQuery,
+        Guid? userId = null)
+    {
+        return query;
+    }
+
+    public virtual IQueryable<T> ApplySorting(
+        IQueryable<T> query,
+        QueryObject queryObject)
+    {
+        if (!string.IsNullOrWhiteSpace(queryObject.SortBy))
+        {
+            return queryObject.Descending
+                ? query.OrderByDescending(e => EF.Property<object>(e, queryObject.SortBy))
+                : query.OrderBy(e => EF.Property<object>(e, queryObject.SortBy));
+        }
+
+        return query;
+    }
+}
---
+++ src/MyPortfolio.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.Domain.Common.ValueObjects;
+using MyPortfolio.Domain.Users.Entities;
+using MyPortfolio.Domain.Users.Interface;
+using MyPortfolio.Infrastructure.Repositories.Base;
+using MyPortfolio.SharedKernel.Helpers;
+
+namespace MyPortfolio.Infrastructure.Repositories;
+
+internal sealed class UserRepository : Repository<User>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext context) 
+        : base(context)
+    {
+    }
+
+    public override IQueryable<User> BuildQuery(ApplicationDbContext context, QueryObject query)
+    {
+        var q = context.Users
+            .Include(p => p.Roles)
+            .Include(u => u.RefreshTokens)
+            .AsQueryable();
+
+        return q;
+    }
+
+    public override async Task AddAsync(
+        User user,
+        CancellationToken cancellationToken = default)
+    {
+        foreach (var role in user.Roles) 
+        {
+            _context.Attach(role);
+        }
+
+        await _context.AddAsync(user, cancellationToken);
+    }
+
+    public async Task<User?> GetByEmailAsync(
+    EmailAddress email,
+    CancellationToken cancellationToken = default)
+    {
+        var query = BuildQuery(_context, null); 
+
+        return await query
+            .FirstOrDefaultAsync(u => u.Email.Value == email.Value, cancellationToken);
+    }
+
+}
