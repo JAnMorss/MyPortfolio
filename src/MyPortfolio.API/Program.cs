805be123fc8330e2cef1f4d4c76b357d4d02d165
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-23 17:31:17 +0800

    Update Program.cs startup configuration

--- src/MyPortfolio.API/Program.cs
+++ src/MyPortfolio.API/Program.cs
@@ -1,4 +1,6 @@
+using Asp.Versioning.ApiExplorer;
 using MyPortfolio.API.Extensions;
+using MyPortfolio.API.SwaggerConfig;
 using MyPortfolio.Application;
 using MyPortfolio.Infrastructure;
 using Serilog;
@@ -19,12 +21,27 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+//builder.Services.AddSwaggerDocumentation();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
+
+    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+    app.UseSwaggerUI(c =>
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            c.SwaggerEndpoint(
+                $"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 
     app.ApplyMigrations();
 }
