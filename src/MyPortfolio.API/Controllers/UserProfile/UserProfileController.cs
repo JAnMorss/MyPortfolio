4dcb027ed842d7e3231d34639d3e8810f0f30ef7
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-24 20:23:06 +0800

    Add UserProfile API endpoints

---
+++ src/MyPortfolio.API/Controllers/UserProfile/UserProfileController.cs
@@ -0,0 +1,39 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.API.Abstractions;
+using MyPortfolio.Application.UserProfile.Queries.GetProfile;
+using MyPortfolio.Application.UserProfile.Responses;
+
+namespace MyPortfolio.API.Controllers.UserProfile;
+
+[ApiController]
+[ApiVersion(ApiVersions.V1)]
+[Route("api/v{version:apiVersion}/user-profile")]
+[Authorize]
+public class UserProfileController : ApiController
+{
+    public UserProfileController(ISender sender) : base(sender)
+    {
+    }
+
+    [HttpGet("me")]
+    public async Task<IActionResult> GetUserProfile(CancellationToken cancellationToken)
+    {
+        var userId = GetUserId();
+        if (userId is null)
+        {
+            return Unauthorized();
+        }
+        var query = new GetProfileQuery(userId.Value);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse<UserResponse>(
+                result.Value, 
+                "User profile retrieved successfully"))
+            : HandleFailure(result);
+    }
+}
