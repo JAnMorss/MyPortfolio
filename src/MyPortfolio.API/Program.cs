84dcec6417670b5dba3402b87d2342228520241a
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-25 21:21:33 +0800

    refactor(api): update Program configuration

--- src/MyPortfolio.API/Program.cs
+++ src/MyPortfolio.API/Program.cs
@@ -1,10 +1,9 @@
-using Asp.Versioning.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using MyPortfolio.API.Extensions;
-using MyPortfolio.API.Swagger;
 using MyPortfolio.Application;
 using MyPortfolio.Infrastructure;
 using MyPortfolio.Infrastructure.Seeding;
+using Scalar.AspNetCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,16 +14,10 @@ builder.Host.UseSerilog((context, loggerConfig) =>
 });
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi("v1");
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
-//builder.Services.AddSwaggerDocumentation();
 
 var app = builder.Build();
 
@@ -32,26 +25,34 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-    dbContext.Database.Migrate();
+    try
+    {
+        dbContext.Database.Migrate();
 
-    await AdminSeeder.SeedAsync(dbContext);
+        AdminSeeder.SeedAsync(dbContext).GetAwaiter().GetResult();
+
+        Console.WriteLine("Database migrated and seeded successfully.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"DB migration/seeding failed: {ex.Message}");
+    }
 }
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-
-    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-
-    app.UseSwaggerUI(c =>
+    app.MapScalarApiReference(options =>
     {
-        foreach (var description in provider.ApiVersionDescriptions)
+        options.Title = "MyPortfolio API Reference";
+        options.DarkMode = true;
+        options.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+        options.CustomCss = "";
+        options.AddPreferredSecuritySchemes("Bearer")
+        .AddHttpAuthentication("Bearer", auth =>
         {
-            c.SwaggerEndpoint(
-                $"/swagger/{description.GroupName}/swagger.json",
-                description.GroupName.ToUpperInvariant());
-        }
+            auth.Token = "jwt-token-placeholder";
+        }).EnablePersistentAuthentication();
     });
 
     app.ApplyMigrations();
