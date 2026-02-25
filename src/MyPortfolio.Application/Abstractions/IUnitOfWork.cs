88f442863976d74dd70b92afce03c86914186ab2
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-25 21:21:42 +0800

    refactor(application): update IUnitOfWork abstraction

--- src/MyPortfolio.Application/Abstractions/IUnitOfWork.cs
+++ src/MyPortfolio.Application/Abstractions/IUnitOfWork.cs
@@ -1,6 +1,10 @@
-﻿namespace MyPortfolio.Application.Abstractions;
+﻿using MyPortfolio.Domain.Users.Entities;
+
+namespace MyPortfolio.Application.Abstractions;
 
 public interface IUnitOfWork
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task AddRefreshTokenAsync(RefreshToken refreshToken, CancellationToken cancellationToken);
 }
