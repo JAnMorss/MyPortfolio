b9ed24883a7c5bcc94ef39fd47ae202717255bc6
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:07:06 +0800

    Add GetEducationById query and handler

---
+++ src/MyPortfolio.Application/Educations/Queries/GetEducationById/GetEducationByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MyPortfolio.Application.Educations.Responses;
+using MyPortfolio.SharedKernel.Mediators.Query;
+
+namespace MyPortfolio.Application.Educations.Queries.GetEducationById;
+
+public sealed record GetEducationByIdQuery(Guid Id) : IQuery<EducationResponse>;
---
+++ src/MyPortfolio.Application/Educations/Queries/GetEducationById/GetEducationByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MyPortfolio.Application.Educations.Responses;
+using MyPortfolio.Domain.Educations.Errors;
+using MyPortfolio.Domain.Educations.Interface;
+using MyPortfolio.SharedKernel.ErrorHandling;
+using MyPortfolio.SharedKernel.Mediators.Query;
+
+namespace MyPortfolio.Application.Educations.Queries.GetEducationById;
+
+public sealed class GetEducationByIdQueryHandler : IQueryHandler<GetEducationByIdQuery, EducationResponse>
+{
+    private readonly IEducationRepository _educationRepository;
+
+    public GetEducationByIdQueryHandler(IEducationRepository educationRepository)
+    {
+        _educationRepository = educationRepository;
+    }
+
+    public async Task<Result<EducationResponse>> Handle(
+        GetEducationByIdQuery request,
+        CancellationToken cancellationToken
+    )
+    {
+        var education = await _educationRepository.GetByIdAsync(request.Id, cancellationToken);
+        if(education is null)
+            return Result.Failure<EducationResponse>(EducationErrors.NotFound);
+
+        return EducationResponse.FromEntity(education);
+    }
+}
