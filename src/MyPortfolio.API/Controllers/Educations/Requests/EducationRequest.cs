9d5ed1469207265aa09633cb6f562a4f2ed83931
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:06 +0800

    Add EducationRequest DTO for creating Education

---
+++ src/MyPortfolio.API/Controllers/Educations/Requests/EducationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPortfolio.API.Controllers.Educations.Requests;
+
+public sealed record EducationRequest(
+    string School,
+    string Degree,
+    DateTime? StartDate,
+    DateTime? EndDate,
+    string? Description);
