5b6592623b1bdae87f36d78d76520353c44f7454
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:18 +0800

    Add EducationErrors for domain validations

---
+++ src/MyPortfolio.Domain/Educations/Errors/EducationErrors.cs
@@ -0,0 +1,18 @@
+﻿using MyPortfolio.SharedKernel.ErrorHandling;
+
+namespace MyPortfolio.Domain.Educations.Errors;
+
+public static class EducationErrors
+{
+    public static readonly Error NotFound = new(
+        "Education.NotFound",
+        "The Education with the specified identifier was not found.");
+
+    public static readonly Error StartDateRequired = new(
+        "Education.StartDateRequired",
+        "The Start Date is required and cannot be null.");
+
+    public static readonly Error EndDateBeforeStartDate = new(
+        "Education.EndDateBeforeStartDate",
+        "The End Date cannot be earlier than the Start Date.");
+}
