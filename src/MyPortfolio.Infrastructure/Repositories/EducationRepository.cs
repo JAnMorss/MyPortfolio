584b2495415b154009d5261ae8611a20032c5003
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:21 +0800

    Add EducationRepository implementation

---
+++ src/MyPortfolio.Infrastructure/Repositories/EducationRepository.cs
@@ -0,0 +1,46 @@
+﻿using MyPortfolio.Domain.Educations.Entities;
+using MyPortfolio.Domain.Educations.Interface;
+using MyPortfolio.Infrastructure.Repositories.Base;
+using MyPortfolio.SharedKernel.Helpers;
+
+namespace MyPortfolio.Infrastructure.Repositories;
+
+internal sealed class EducationRepository
+    : Repository<Education>, IEducationRepository
+{
+    public EducationRepository(ApplicationDbContext context)
+        : base(context)
+    {
+    }
+
+    public override IQueryable<Education> ApplySorting(
+        IQueryable<Education> query,
+        QueryObject queryObject)
+    {
+        if (string.IsNullOrWhiteSpace(queryObject.SortBy))
+            return query;
+
+        query = queryObject.SortBy.ToLower() switch
+        {
+            "school" => queryObject.Descending
+                ? query.OrderByDescending(e => e.School.Value)
+                : query.OrderBy(e => e.School.Value),
+
+            "degree" => queryObject.Descending
+                ? query.OrderByDescending(e => e.Degree.Value)
+                : query.OrderBy(e => e.Degree.Value),
+
+            "startdate" => queryObject.Descending
+                ? query.OrderByDescending(e => e.StartDate)
+                : query.OrderBy(e => e.StartDate),
+
+            "enddate" => queryObject.Descending
+                ? query.OrderByDescending(e => e.EndDate)
+                : query.OrderBy(e => e.EndDate),
+
+            _ => query
+        };
+
+        return query;
+    }
+}
