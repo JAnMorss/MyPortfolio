900e03eadd4cea557622e67d1a5a7de820f99563
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:16 +0800

    Add EducationResponse DTO

---
+++ src/MyPortfolio.Application/Educations/Responses/EducationResponse.cs
@@ -0,0 +1,27 @@
+﻿using MyPortfolio.Domain.Educations.Entities;
+
+namespace MyPortfolio.Application.Educations.Responses;
+
+public sealed class EducationResponse
+{
+    public string? School { get; set; }
+    public string? Degree { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? Description { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public static EducationResponse FromEntity(Education education)
+    {
+        return new EducationResponse
+        {
+            School = education.School?.Value,
+            Degree = education.Degree?.Value,
+            StartDate = education.StartDate,
+            EndDate = education.EndDate,
+            Description = education.Description?.Value,
+            UserId = education.UserId
+        };
+    }
+}
