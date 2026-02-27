2214c368a8e1d9797315fa08f7969ab33f7778f5
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:11 +0800

    Add CreateEducationCommand record

---
+++ src/MyPortfolio.Application/Educations/Commands/CreateEducation/CreateEducationCommand.cs
@@ -0,0 +1,13 @@
+﻿using MyPortfolio.Application.Educations.Responses;
+using MyPortfolio.SharedKernel.Mediators.Command;
+
+namespace MyPortfolio.Application.Educations.Commands.CreateEducation;
+
+public sealed record CreateEducationCommand(
+    string School,
+    string Degree,
+    DateTime? StartDate,
+    DateTime? EndDate,
+    string? Description,
+    Guid UserId
+) : ICommand<EducationResponse>;
