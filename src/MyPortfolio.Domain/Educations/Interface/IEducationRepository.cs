c91dfc8b68567cad884d4cb94960b7d7fd5bd930
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:20 +0800

    Add IEducationRepository interface

---
+++ src/MyPortfolio.Domain/Educations/Interface/IEducationRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyPortfolio.Domain.Educations.Entities;
+using MyPortfolio.SharedKernel.Repositories;
+
+namespace MyPortfolio.Domain.Educations.Interface;
+
+public interface IEducationRepository : IRepository<Education>
+{
+}
