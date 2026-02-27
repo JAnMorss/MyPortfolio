2eb5cc65abf6d653b6e18e508e64e0c182716e8a
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-27 21:06:13 +0800

    Add GetAllEducationsQuery record

---
+++ src/MyPortfolio.Application/Educations/Queries/GetAllEducations/GetAllEducationsQuery.cs
@@ -0,0 +1,11 @@
+﻿using MyPortfolio.Application.Abstractions.PageSize;
+using MyPortfolio.Application.Educations.Responses;
+using MyPortfolio.SharedKernel.Helpers;
+using MyPortfolio.SharedKernel.Mediators.Query;
+
+namespace MyPortfolio.Application.Educations.Queries.GetAllEducations;
+
+public sealed record GetAllEducationsQuery(
+    QueryObject? Query,
+    Guid UserId
+) : IQuery<PaginatedResult<EducationResponse>>;
