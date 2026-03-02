15a94131056dea089af549196e5cda736636e7f2
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-02 17:24:34 +0800

    Add new UserProfile request DTOs

---
+++ src/MyPortfolio.API/Controllers/UserProfile/Requests/UserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolio.API.Controllers.UserProfile.Requests;
+
+public sealed record UserRequest(
+    string FirstName,
+    string LastName,
+    int Age,
+    string HeadLine,
+    string About
+);
