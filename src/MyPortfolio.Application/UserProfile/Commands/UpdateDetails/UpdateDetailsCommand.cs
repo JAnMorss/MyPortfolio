c0fd6e94b463dc049a8d894203de116ad5913f60
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-02 17:24:37 +0800

    Add UserProfile command handlers

---
+++ src/MyPortfolio.Application/UserProfile/Commands/UpdateDetails/UpdateDetailsCommand.cs
@@ -0,0 +1,13 @@
+﻿using MyPortfolio.Application.UserProfile.Responses;
+using MyPortfolio.SharedKernel.Mediators.Command;
+
+namespace MyPortfolio.Application.UserProfile.Commands.UpdateDetails;
+
+public sealed record UpdateDetailsCommand(
+    Guid UserId,
+    string FirstName,
+    string LastName,
+    int Age,
+    string HeadLine,
+    string About
+) : ICommand<UserResponse>;
---
+++ src/MyPortfolio.Application/UserProfile/Commands/UpdateDetails/UpdateDetailsCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using MyPortfolio.Application.Abstractions;
+using MyPortfolio.Application.UserProfile.Responses;
+using MyPortfolio.Domain.Users.Errors;
+using MyPortfolio.Domain.Users.Interface;
+using MyPortfolio.SharedKernel.ErrorHandling;
+using MyPortfolio.SharedKernel.Mediators.Command;
+
+namespace MyPortfolio.Application.UserProfile.Commands.UpdateDetails;
+
+public sealed class UpdateDetailsCommandHandler 
+    : ICommandHandler<UpdateDetailsCommand, UserResponse>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateDetailsCommandHandler(
+        IUserRepository userRepository, 
+        IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<UserResponse>> Handle(
+        UpdateDetailsCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+        if(user is null)
+            return Result.Failure<UserResponse>(UserErrors.NotFound);
+
+        var updateResult = user.UpdateDetails(
+            request.FirstName, 
+            request.LastName, 
+            request.Age, 
+            request.HeadLine, 
+            request.About
+        );
+
+        if(updateResult.IsFailure)
+            return Result.Failure<UserResponse>(updateResult.Error);
+
+        await _userRepository.UpdateAsync(user, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(UserResponse.FromEntity(user));
+    }
+}
