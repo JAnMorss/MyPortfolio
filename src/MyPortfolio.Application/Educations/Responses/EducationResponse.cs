632422fdc1ebf49c874c05f650a4c185311bf57d
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-02 17:24:20 +0800

    Add CreatedAt and UpdatedAt to EducationResponse

--- src/MyPortfolio.Application/Educations/Responses/EducationResponse.cs
+++ src/MyPortfolio.Application/Educations/Responses/EducationResponse.cs
@@ -4,23 +4,28 @@ namespace MyPortfolio.Application.Educations.Responses;
 
 public sealed class EducationResponse
 {
+    public Guid Id { get; set; }
     public string? School { get; set; }
     public string? Degree { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public string? Description { get; set; }
-
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public Guid UserId { get; set; }
 
     public static EducationResponse FromEntity(Education education)
     {
         return new EducationResponse
         {
+            Id = education.Id,
             School = education.School?.Value,
             Degree = education.Degree?.Value,
             StartDate = education.StartDate,
             EndDate = education.EndDate,
             Description = education.Description?.Value,
+            CreatedAt = education.CreatedAt,
+            UpdatedAt = education.UpdatedAt,
             UserId = education.UserId
         };
     }
