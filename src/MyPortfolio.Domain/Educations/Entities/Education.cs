9a91c1e8ec7793c2bdb595947162cbe13dcb4b2f
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-02 17:24:22 +0800

    Add UpdateEducation method and SetDateRange validation to Education entity

--- src/MyPortfolio.Domain/Educations/Entities/Education.cs
+++ src/MyPortfolio.Domain/Educations/Entities/Education.cs
@@ -1,5 +1,7 @@
-﻿using MyPortfolio.Domain.Common.ValueObjects;
+﻿using MediatR;
+using MyPortfolio.Domain.Common.ValueObjects;
 using MyPortfolio.Domain.Educations.Errors;
+using MyPortfolio.Domain.Educations.Events;
 using MyPortfolio.Domain.Educations.ValueObjects;
 using MyPortfolio.Domain.Users.Entities;
 using MyPortfolio.SharedKernel.Domain;
@@ -24,6 +26,8 @@ public sealed class Education : BaseEntity
         Degree = degree;
         Description = description;
         UserId = userId;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = null;
 
         var result = SetDateRange(startDate, endDate);
     }
@@ -33,9 +37,61 @@ public sealed class Education : BaseEntity
     public DateTime? StartDate { get; private set; }
     public DateTime? EndDate { get; private set; }
     public Description? Description { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
 
     public Guid UserId { get; private set; }
     public User User { get; private set; } = null!;
+
+    public Result UpdateEducation(
+        string school,
+        string degree,
+        DateTime? startDate,
+        DateTime? endDate,
+        string? description)
+    {
+        bool isUpdated = false;
+
+        if (!string.IsNullOrWhiteSpace(school) && school != School?.Value)
+        {
+            var schoolResult = School.Create(school);
+            if (schoolResult.IsFailure)
+                return Result.Failure(schoolResult.Error);
+
+            School = schoolResult.Value;
+            isUpdated = true;
+        }
+
+        if (!string.IsNullOrWhiteSpace(degree) && degree != Degree?.Value)
+        {
+            var degreeResult = Degree.Create(degree);
+            if (degreeResult.IsFailure)
+                return Result.Failure(degreeResult.Error);
+
+            Degree = degreeResult.Value;
+            isUpdated = true;
+        }
+
+        if (!string.IsNullOrWhiteSpace(description) && description != Description?.Value)
+        {
+            var descriptionResult = Description.Create(description);
+            if (descriptionResult.IsFailure)
+                return Result.Failure(descriptionResult.Error);
+
+            Description = descriptionResult.Value;
+            isUpdated = true;
+        }
+
+        var result = SetDateRange(startDate, endDate);
+
+        if (isUpdated)
+        {
+            UpdatedAt = DateTime.UtcNow;
+            RaiseDomainEvent(new EducationUpdatedDomainEvent(Id));
+        }
+
+        return Result.Success(this);
+    }
 
     public Result SetDateRange(DateTime? startDate, DateTime? endDate)
     {
