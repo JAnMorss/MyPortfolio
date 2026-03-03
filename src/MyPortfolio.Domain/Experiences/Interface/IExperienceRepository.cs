4e28d729d72d6259a93fee3d4e0952feee8768ed
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-03 18:22:52 +0800

    Add repository interfaces for Experiences

---
+++ src/MyPortfolio.Domain/Experiences/Interface/IExperienceRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyPortfolio.Domain.Experiences.Entities;
+using MyPortfolio.SharedKernel.Repositories;
+
+namespace MyPortfolio.Domain.Experiences.Interface;
+
+public interface IExperienceRepository : IRepository<Experience>
+{
+}
