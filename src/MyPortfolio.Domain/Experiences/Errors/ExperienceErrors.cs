118c2d5757c52a26b1f806227aeaa9697c47aedd
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-03 18:22:41 +0800

    Add domain errors for Experiences

---
+++ src/MyPortfolio.Domain/Experiences/Errors/ExperienceErrors.cs
@@ -0,0 +1,19 @@
+﻿using MyPortfolio.SharedKernel.ErrorHandling;
+
+namespace MyPortfolio.Domain.Experiences.Errors;
+
+public static class ExperienceErrors
+{
+    public static readonly Error NotFound = new(
+        "Experience.NotFound",
+        "The Experience with the specified identifier was not found.");
+
+    public static readonly Error StartDateRequired = new(
+        "Experience.StartDateRequired",
+        "The Start Date is required and cannot be null.");
+
+    public static readonly Error EndDateBeforeStartDate = new(
+        "Experience.EndDateBeforeStartDate",
+        "The End Date cannot be earlier than the Start Date.");
+}
+
