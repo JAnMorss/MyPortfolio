84ffd70d3ec6fd3d45977a19500c94b25bc21a07
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-03 18:22:07 +0800

    Add Experience entity with domain rules and domain events

--- src/MyPortfolio.Domain/Experiences/Entities/Experience.cs
+++ src/MyPortfolio.Domain/Experiences/Entities/Experience.cs
@@ -1,7 +1,10 @@
 ﻿using MyPortfolio.Domain.Common.ValueObjects;
+using MyPortfolio.Domain.Educations.Errors;
+using MyPortfolio.Domain.Experiences.Events;
 using MyPortfolio.Domain.Experiences.ValueObjects;
 using MyPortfolio.Domain.Users.Entities;
 using MyPortfolio.SharedKernel.Domain;
+using MyPortfolio.SharedKernel.ErrorHandling;
 
 namespace MyPortfolio.Domain.Experiences.Entities;
 
@@ -35,4 +38,61 @@ public sealed class Experience : BaseEntity
 
     public Guid UserId { get; private set; }
     public User User { get; private set; } = null!;
+
+
+    public Result UpdateExperience(
+        string companyName,
+        DateTime? startDate,
+        DateTime? endDate,
+        string? description)
+    {
+        bool isUpdated = false;
+
+        if (!string.IsNullOrWhiteSpace(companyName) && companyName != CompanyName?.Value)
+        {
+            var companyNameResult = CompanyName.Create(companyName);
+            if (companyNameResult.IsFailure)
+                return Result.Failure(companyNameResult.Error);
+
+            CompanyName = companyNameResult.Value;
+            isUpdated = true;
+        }
+
+        var dateRangeResult = SetDateRange(startDate, endDate);
+
+        if (dateRangeResult.IsFailure)
+            return Result.Failure(dateRangeResult.Error);
+
+        if (!string.IsNullOrWhiteSpace(description) && description != Description?.Value)
+        {
+            var descriptionResult = Description.Create(description);
+            if (descriptionResult.IsFailure)
+                return Result.Failure(descriptionResult.Error);
+
+            Description = descriptionResult.Value;
+            isUpdated = true;
+        }
+
+        if (isUpdated)
+        {
+            UpdatedAt = DateTime.UtcNow;
+            RaiseDomainEvent(new ExperienceUpdatedDomainEvent(Id));
+        }
+
+        return Result.Success(this);
+    }
+
+    public Result SetDateRange(DateTime? startDate, DateTime? endDate)
+    {
+        if (!startDate.HasValue)
+            return Result.Failure(EducationErrors.StartDateRequired);
+
+        if (endDate.HasValue && endDate.Value < startDate.Value)
+            return Result.Failure(EducationErrors.EndDateBeforeStartDate);
+
+        StartDate = startDate.Value;
+        EndDate = endDate;
+
+        return Result.Success();
+    }
 }
