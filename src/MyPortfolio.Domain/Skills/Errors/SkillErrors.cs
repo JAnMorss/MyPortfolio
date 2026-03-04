a64f6ebf7042b8a795cde4b3990dff7dcf8e0630
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-04 18:05:48 +0800

    add skill domain errors

---
+++ src/MyPortfolio.Domain/Skills/Errors/SkillErrors.cs
@@ -0,0 +1,11 @@
+﻿using MyPortfolio.SharedKernel.ErrorHandling;
+
+namespace MyPortfolio.Domain.Skills.Errors;
+
+public static class SkillErrors
+{
+    public static readonly Error NotFound = new(
+        "Skill.NotFound",
+        "The Skill with the specified identifier was not found."
+    );
+}
