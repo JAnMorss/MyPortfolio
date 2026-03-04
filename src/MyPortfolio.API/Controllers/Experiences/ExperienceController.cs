5e1aa861547cefff127a0edcce98e180213d84d1
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-04 18:04:09 +0800

    refactor(experience): update ExperienceController

--- src/MyPortfolio.API/Controllers/Experiences/ExperienceController.cs
+++ src/MyPortfolio.API/Controllers/Experiences/ExperienceController.cs
@@ -3,7 +3,11 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.API.Abstractions;
+using MyPortfolio.API.Controllers.Experiences.Requests;
 using MyPortfolio.Application.Abstractions.PageSize;
+using MyPortfolio.Application.Experiences.Commands.CreateExperience;
+using MyPortfolio.Application.Experiences.Commands.DeleteExperience;
+using MyPortfolio.Application.Experiences.Commands.UpdateExperience;
 using MyPortfolio.Application.Experiences.Queries.GetAllExperiences;
 using MyPortfolio.Application.Experiences.Queries.GetExperienceById;
 using MyPortfolio.Application.Experiences.Responses;
@@ -55,6 +59,69 @@ public class ExperienceController : ApiController
             ? Ok(new ApiResponse<ExperienceResponse>(
                 result.Value,
                 "Fetching experience by id is successfull."))
+            : HandleFailure(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateExperience(
+        [FromBody] ExperienceRequest request,
+        CancellationToken cancellationToken)
+    {
+        var userId = GetUserId();
+        if(userId is null) 
+            return Unauthorized();
+
+        var command = new CreateExperienceCommand(
+            request.CompanyName,
+            request.StartDate,
+            request.EndDate,
+            request.Description,
+            userId.Value
+        );
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse<ExperienceResponse>(
+                result.Value,
+                "Experience created successfully."))
+            : HandleFailure(result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateExperience(
+        [FromRoute] Guid id,
+        [FromBody] ExperienceRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateExperienceCommand(
+            id,
+            request.CompanyName,
+            request.StartDate,
+            request.EndDate,
+            request.Description
+        );
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse<ExperienceResponse>(
+                result.Value,
+                "Experience updated successfully."))
+            : HandleFailure(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteExperience(
+        [FromRoute] Guid id,
+        CancellationToken cancellationToken)
+    {
+        var command = new DeleteExperienceCommand(id);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse("Experience deleted successfully."))
             : HandleFailure(result);
     }
 }
