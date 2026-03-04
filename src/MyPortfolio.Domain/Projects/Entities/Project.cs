773c4e1f0e7a1a890c1959c6720b00025927f782
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-04 18:05:07 +0800

    update Project entity

--- src/MyPortfolio.Domain/Projects/Entities/Project.cs
+++ src/MyPortfolio.Domain/Projects/Entities/Project.cs
@@ -1,7 +1,10 @@
 ﻿using MyPortfolio.Domain.Common.ValueObjects;
+using MyPortfolio.Domain.Experiences.ValueObjects;
+using MyPortfolio.Domain.Projects.Events;
 using MyPortfolio.Domain.Projects.ValueObjects;
 using MyPortfolio.Domain.Users.Entities;
 using MyPortfolio.SharedKernel.Domain;
+using MyPortfolio.SharedKernel.ErrorHandling;
 
 namespace MyPortfolio.Domain.Projects.Entities;
 
@@ -38,4 +41,61 @@ public sealed class Project : BaseEntity
 
     public Guid UserId { get; private set; }
     public User User { get; private set; } = null!;
+
+    public Result Update(
+        string title,
+        string? description,
+        string techstack,
+        string? link)
+    {
+        bool isUpdated = false;
+
+        if (!string.IsNullOrWhiteSpace(title) && title != Title?.Value)
+        {
+            var titleResult = Title.Create(title);
+            if (titleResult.IsFailure)
+                return Result.Failure(titleResult.Error);
+
+            Title = titleResult.Value;
+            isUpdated = true;
+        }
+
+        if (!string.IsNullOrWhiteSpace(description) && description != Description?.Value)
+        {
+            var descriptionResult = Description.Create(description);
+            if (descriptionResult.IsFailure)
+                return Result.Failure(descriptionResult.Error);
+
+            Description = descriptionResult.Value;
+            isUpdated = true;
+        }
+
+        if (!string.IsNullOrWhiteSpace(techstack) && techstack != Techstack?.Value)
+        {
+            var techstackResult = Techstack.Create(techstack);
+            if (techstackResult.IsFailure)
+                return Result.Failure(techstackResult.Error);
+
+            Techstack = techstackResult.Value;
+            isUpdated = true;
+        }
+
+        if (!string.IsNullOrWhiteSpace(link) && link != Link?.Value)
+        {
+            var linkResult = Link.Create(link);
+            if (linkResult.IsFailure)
+                return Result.Failure(linkResult.Error);
+
+            Link = linkResult.Value;
+            isUpdated = true;
+        }
+
+        if(isUpdated)
+        {
+            UpdatedAt = DateTime.UtcNow;
+            RaiseDomainEvent(new ProjectUpdatedDomainEvent(Id));
+        }
+
+        return Result.Success(this);
+    }
 }
