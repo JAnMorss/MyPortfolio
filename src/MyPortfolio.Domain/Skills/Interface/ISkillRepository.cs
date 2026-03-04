a6181ecfebcb6fbb9c7903ae03b5360dc26fddae
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-04 18:06:15 +0800

    add skill repository interface

---
+++ src/MyPortfolio.Domain/Skills/Interface/ISkillRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyPortfolio.Domain.Skills.Entities;
+using MyPortfolio.SharedKernel.Repositories;
+
+namespace MyPortfolio.Domain.Skills.Interface;
+
+public interface ISkillRepository : IRepository<Skill>
+{
+}
