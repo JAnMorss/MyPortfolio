156d123e3655da76ea78f5115997c7dd92e46921
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-04 18:05:34 +0800

    add/update Skill entity

--- src/MyPortfolio.Domain/Skills/Entities/Skill.cs
+++ src/MyPortfolio.Domain/Skills/Entities/Skill.cs
@@ -1,7 +1,10 @@
-﻿using MyPortfolio.Domain.Skills.Enums;
+﻿using MyPortfolio.Domain.Projects.ValueObjects;
+using MyPortfolio.Domain.Skills.Enums;
+using MyPortfolio.Domain.Skills.Events;
 using MyPortfolio.Domain.Skills.ValueObjects;
 using MyPortfolio.Domain.Users.Entities;
 using MyPortfolio.SharedKernel.Domain;
+using MyPortfolio.SharedKernel.ErrorHandling;
 
 namespace MyPortfolio.Domain.Skills.Entities;
 
@@ -29,4 +32,37 @@ public sealed class Skill : BaseEntity
 
     public Guid UserId { get; private set; }
     public User User { get; private set; } = null!;
+
+    public Result Update(
+        string skillName,
+        Level? level)
+    {
+        bool isUpdated = false;
+
+        if(!string.IsNullOrWhiteSpace(skillName) && skillName != SkillName?.Value)
+        {
+            var skillNameResult = SkillName.Create(skillName);
+            if (skillNameResult.IsFailure)
+                return Result.Failure(skillNameResult.Error);
+
+            SkillName = skillNameResult.Value;
+
+            isUpdated = true;
+        }
+
+        if(level != Level)
+        {
+            Level = level;
+
+            isUpdated = true;
+        }
+
+        if (isUpdated)
+        {
+            UpdatedAt = DateTime.UtcNow;
+            RaiseDomainEvent(new SkillUpdatedDomainEvent(Id));
+        }
+
+        return Result.Success(this);
+    }
 }
