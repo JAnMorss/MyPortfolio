2efa73246d1cb751841ecff8da1877cc77a4060a
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-04 18:04:25 +0800

    refactor(education): update DeleteEducationCommandHandler

--- src/MyPortfolio.Application/Educations/Commands/DeleteEducation/DeleteEducationCommandHandler.cs
+++ src/MyPortfolio.Application/Educations/Commands/DeleteEducation/DeleteEducationCommandHandler.cs
@@ -6,7 +6,7 @@ using MyPortfolio.SharedKernel.Mediators.Command;
 
 namespace MyPortfolio.Application.Educations.Commands.DeleteEducation;
 
-public sealed class DeleteEducationCommandHandler : ICommandHandler<DeleteEducationCommand, Guid>
+public sealed class DeleteEducationCommandHandler : ICommandHandler<DeleteEducationCommand>
 {
     private readonly IEducationRepository _educationRepository;
     private readonly IUnitOfWork _unitOfWork;
@@ -19,18 +19,18 @@ public sealed class DeleteEducationCommandHandler : ICommandHandler<DeleteEducat
         _unitOfWork = unitOfWork;
     }
 
-    public async Task<Result<Guid>> Handle(
+    public async Task<Result> Handle(
         DeleteEducationCommand request, 
         CancellationToken cancellationToken)
     {
         var education = await _educationRepository.GetByIdAsync(request.Id, cancellationToken);
         if (education is null)
-            return Result.Failure<Guid>(EducationErrors.NotFound);
+            return Result.Failure(EducationErrors.NotFound);
 
         await _educationRepository.DeleteAsync(education.Id, cancellationToken);
 
         await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return Result.Success(education.Id);
+        return Result.Success();
     }
 }
