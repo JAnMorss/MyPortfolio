3d1c14c681d4352e540ca79518e5b78b2f941afe
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-06 20:14:10 +0800

    Add project repository interface

---
+++ src/MyPortfolio.Domain/Projects/Interface/IProjectRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyPortfolio.Domain.Projects.Entities;
+using MyPortfolio.SharedKernel.Repositories;
+
+namespace MyPortfolio.Domain.Projects.Interface;
+
+public interface IProjectRepository : IRepository<Project>
+{
+}
