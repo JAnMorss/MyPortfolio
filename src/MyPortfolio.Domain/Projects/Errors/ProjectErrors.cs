d50c9d755e52602dff1e1bf8cb57ea38253f2ec0
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-06 20:14:05 +0800

    Add project domain errors

---
+++ src/MyPortfolio.Domain/Projects/Errors/ProjectErrors.cs
@@ -0,0 +1,10 @@
+﻿using MyPortfolio.SharedKernel.ErrorHandling;
+
+namespace MyPortfolio.Domain.Projects.Errors;
+
+public static class ProjectErrors
+{
+    public static readonly Error NotFound = new(
+        "Project.NotFound",
+        "The Project with the specified identifier was not found.");
+}
