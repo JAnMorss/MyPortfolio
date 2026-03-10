f399a7d3f987d23b483afaebbf87ec34cb549b15
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-10 20:12:22 +0800

    feat(user-avatar): implement upload user avatar command

---
+++ src/MyPortfolio.Application/UserProfile/Commands/UploadUserAvatar/UploadUserAvatarCommand.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using MyPortfolio.SharedKernel.Mediators.Command;
+
+namespace MyPortfolio.Application.UserProfile.Commands.UploadUserAvatar;
+
+public sealed record UploadUserAvatarCommand(
+    Guid UserId,
+    IFormFile File
+) : ICommand;
---
+++ src/MyPortfolio.Application/UserProfile/Commands/UploadUserAvatar/UploadUserAvatarCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using MyPortfolio.Application.Abstractions;
+using MyPortfolio.Application.Abstractions.BlobStorage;
+using MyPortfolio.Domain.Common.ValueObjects;
+using MyPortfolio.Domain.Users.Errors;
+using MyPortfolio.Domain.Users.Interface;
+using MyPortfolio.SharedKernel.ErrorHandling;
+using MyPortfolio.SharedKernel.Mediators.Command;
+
+namespace MyPortfolio.Application.UserProfile.Commands.UploadUserAvatar;
+
+public sealed class UploadUserAvatarCommandHandler
+    : ICommandHandler<UploadUserAvatarCommand>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IBlobService _blobService;
+
+    private const string AvatarContainer = "avatars";
+
+    public UploadUserAvatarCommandHandler(
+        IUserRepository userRepository,
+        IUnitOfWork unitOfWork,
+        IBlobService blobService)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+        _blobService = blobService;
+    }
+    public async Task<Result> Handle(
+        UploadUserAvatarCommand request,
+        CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+        if (user is null)
+            return Result.Failure(UserErrors.NotFound);
+
+        if(user.Photo is not null)
+        {
+            var existingFileId = Photo.ExtractFileIdFromUrl(user.Photo.Value);
+            if (existingFileId.HasValue)
+                await _blobService.DeleteAsync(AvatarContainer, existingFileId.Value);
+        }
+
+        using var stream = request.File.OpenReadStream();
+
+        var newFileId = await _blobService.UploadAsync(
+            AvatarContainer,
+            stream,
+            request.File.ContentType,
+            cancellationToken
+        );
+
+        var newAvatarUrl = _blobService.GetBlobUri(AvatarContainer, newFileId);
+
+        var avatarResult = user.UpdateAvatar(newAvatarUrl);
+        if(avatarResult.IsFailure)
+            return Result.Failure(avatarResult.Error);
+
+        await _userRepository.UpdateAsync(user, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
