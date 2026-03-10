024b627506397cde79c06ee3d4ee8d1ab7c14ad9
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-10 20:13:44 +0800

    add endpoints for uploading and retrieving user avatar

--- src/MyPortfolio.API/Controllers/UserProfile/UserProfileController.cs
+++ src/MyPortfolio.API/Controllers/UserProfile/UserProfileController.cs
@@ -5,7 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.API.Abstractions;
 using MyPortfolio.API.Controllers.UserProfile.Requests;
 using MyPortfolio.Application.UserProfile.Commands.UpdateDetails;
+using MyPortfolio.Application.UserProfile.Commands.UploadUserAvatar;
 using MyPortfolio.Application.UserProfile.Queries.GetProfile;
+using MyPortfolio.Application.UserProfile.Queries.GetUserAvatar;
 using MyPortfolio.Application.UserProfile.Responses;
 
 namespace MyPortfolio.API.Controllers.UserProfile;
@@ -62,6 +64,42 @@ public class UserProfileController : ApiController
             ? Ok(new ApiResponse<UserResponse>(
                 result.Value, 
                 "User profile updated successfully"))
+            : HandleFailure(result);
+    }
+
+    [HttpGet("avatar")]
+    public async Task<IActionResult> GetUserAvatar(CancellationToken cancellationToken)
+    {
+        var userId = GetUserId();
+        if (userId is null)
+            return Unauthorized();
+
+        var query = new GetUserAvatarQuery(userId.Value);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse<UserAvatarResponse>(
+                result.Value,
+                "User Avatar fetched successfully"))
+            : HandleFailure(result);
+    }
+
+    [HttpPut("avatar")]
+    public async Task<IActionResult> UploadAvatar(
+        IFormFile file,
+        CancellationToken cancellationToken)
+    {
+        var userId = GetUserId();
+        if (userId is null)
+            return Unauthorized();
+
+        var command = new UploadUserAvatarCommand(userId.Value, file);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse("User avatar updated successfully"))
             : HandleFailure(result);
     }
 }
