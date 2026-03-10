f4b8edd521b40ebec346d9ae051f6da6893d5598
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-10 20:12:45 +0800

    add user avatar response model

---
+++ src/MyPortfolio.Application/UserProfile/Responses/UserAvatarResponse.cs
@@ -0,0 +1,25 @@
+﻿using MyPortfolio.Domain.Users.Entities;
+
+namespace MyPortfolio.Application.UserProfile.Responses;
+
+public sealed class UserAvatarResponse
+{
+    public string? AvatarUrl { get; init; } = string.Empty;
+
+    public byte[]? ImageBytes { get; init; }
+
+    public string? ContentType { get; init; }
+
+    public static UserAvatarResponse FromEntity(
+        User user,
+        byte[]? imageBytes = null,
+        string? contentType = null)
+    {
+        return new UserAvatarResponse
+        {
+            AvatarUrl = user.Photo?.Value,
+            ImageBytes = imageBytes,
+            ContentType = contentType
+        };
+    }
+}
