e84589f01b9943304ef2f398770b0581933edfc6
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-11 16:55:33 +0800

    Rename ImageUrl column to MediaUrl in Projects table

---
+++ src/MyPortfolio.Infrastructure/Migrations/20260311070942_RenameProjectImageUrlToMediaUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameProjectImageUrlToMediaUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Projects",
+                newName: "MediaUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MediaUrl",
+                table: "Projects",
+                newName: "ImageUrl");
+        }
+    }
+}
