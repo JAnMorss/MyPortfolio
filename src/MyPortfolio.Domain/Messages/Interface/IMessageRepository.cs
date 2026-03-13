432e2a06e775face836d4c4bc643cc7b4f1294a1
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-13 17:41:15 +0800

    Add IMessageRepository interface for Message module

---
+++ src/MyPortfolio.Domain/Messages/Interface/IMessageRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyPortfolio.Domain.Messages.Entities;
+using MyPortfolio.SharedKernel.Repositories;
+
+namespace MyPortfolio.Domain.Messages.Interface;
+
+public interface IMessageRepository : IRepository<Message>
+{
+    Task<int> CountMessagesSentTodayAsync(string personName, string? email, CancellationToken cancellationToken = default);
+}
