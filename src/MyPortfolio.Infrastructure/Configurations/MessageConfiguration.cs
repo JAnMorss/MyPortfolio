1135300b11869c086571755f6a916e17b8213ad3
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-13 17:41:04 +0800

    Update EF Core Message configuration, make Email and PhoneNumber optional

--- src/MyPortfolio.Infrastructure/Configurations/MessageConfiguration.cs
+++ src/MyPortfolio.Infrastructure/Configurations/MessageConfiguration.cs
@@ -15,32 +15,36 @@ internal sealed class MessageConfiguration : IEntityTypeConfiguration<Message>
         builder.OwnsOne(m => m.PersonName, pn =>
         {
             pn.Property(p => p.Value)
-                .HasColumnName("PersonName")
-                .HasMaxLength(150);
+              .HasColumnName("PersonName")
+              .HasMaxLength(150)
+              .IsRequired();
         });
 
         builder.OwnsOne(m => m.Email, e =>
         {
             e.Property(p => p.Value)
-                .HasColumnName("Email")
-                .HasMaxLength(255);
+              .HasColumnName("Email")
+              .HasMaxLength(255)
+              .IsRequired(false);
         });
 
         builder.OwnsOne(m => m.PhoneNumber, e =>
         {
             e.Property(p => p.Value)
-                .HasColumnName("PhoneNumber")
-                .HasMaxLength(16);
+              .HasColumnName("PhoneNumber")
+              .HasMaxLength(16)
+              .IsRequired(false);
         });
 
         builder.OwnsOne(m => m.Content, e =>
         {
             e.Property(p => p.Value)
-                .HasColumnName("Content")
-                .HasMaxLength(1000);
+              .HasColumnName("Content")
+              .HasMaxLength(1000)
+              .IsRequired();
         });
 
         builder.Property(m => m.SentAt)
-            .IsRequired();
+               .IsRequired();
     }
 }
