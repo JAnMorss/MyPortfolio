e10f9f6f03a8fa91253641016172efb0698a9ba9
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-13 17:41:14 +0800

    Add domain errors for Message module

---
+++ src/MyPortfolio.Domain/Messages/Errors/MessageErrors.cs
@@ -0,0 +1,17 @@
+﻿using MyPortfolio.SharedKernel.ErrorHandling;
+
+namespace MyPortfolio.Domain.Messages.Errors;
+
+public static class MessageErrors
+{
+    public static readonly Error NotFound = new(
+        "Message.NotFound",
+        "The Message with the specified identifier was not found."
+    );
+
+    public static readonly Error LimitExceeded = new(
+        "Message.LimitExceeded",
+        "You can only send 1 messages per day."
+    );
+     
+}
