8e5cf95fd4c54010e9838d0161d34f754bc6ce37
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-20 19:48:26 +0800

    Update App.tsx

--- src/MyPortfolio.Client/src/App.tsx
+++ src/MyPortfolio.Client/src/App.tsx
@@ -1,33 +1,24 @@
 import Header from './components/header/Header'
+import SidebarProfile from './components/sidebarProfile/sidebarProfile'
 
 function App() {
 
   return (
     <>
-        <div className="min-h-screen bg-white text-black dark:bg-[#0d1117] dark:text-white">
+        <div className="min-h-screen bg-white text-black dark:bg-[#212830] dark:text-white">
           <Header />
+          <div className="max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="flex flex-col lg:flex-row gap-6">
+              <div className="lg:w-[296px]">
+                <SidebarProfile />
+              </div>
+
+            </div>
+          </div>
         </div>
 
       
-      {/* <a href="https://github.com/vitejs/vite" target="_blank">
-        <svg
-          className="button-icon"
-          role="presentation"
-          aria-hidden="true"
-        >
-          <use href="/icons.svg#github-icon"></use>
-        </svg>
-      </a> 
       
-      <a href="https://chat.vite.dev/" target="_blank">
-        <svg
-          className="button-icon"
-          role="presentation"
-          aria-hidden="true"
-        >
-          <use href="/icons.svg#discord-icon"></use>
-        </svg>
-      </a> */}
     </>
   )
 }
