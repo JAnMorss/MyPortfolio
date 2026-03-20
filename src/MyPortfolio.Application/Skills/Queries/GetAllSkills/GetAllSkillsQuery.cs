f07c6d7b0812b9ef9289425ff78e712435eaa722
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-20 19:48:09 +0800

    Update GetAllSkillsQuery.cs

--- src/MyPortfolio.Application/Skills/Queries/GetAllSkills/GetAllSkillsQuery.cs
+++ src/MyPortfolio.Application/Skills/Queries/GetAllSkills/GetAllSkillsQuery.cs
@@ -6,7 +6,4 @@ using MyPortfolio.SharedKernel.Mediators.Query;
 
 namespace MyPortfolio.Application.Skills.Queries.GetAllSkills;
 
-public sealed record GetAllSkillsQuery(
-    QueryObject? Query,
-    Guid UserId
-) : IQuery<PaginatedResult<SkillResponse>>;
+public sealed record GetAllSkillsQuery(QueryObject? Query) : IQuery<PaginatedResult<SkillResponse>>;
