664308a05798cde7c28e1781b467c1e73d4b3913
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-20 19:48:26 +0800

    Update Header.tsx

--- src/MyPortfolio.Client/src/components/header/Header.tsx
+++ src/MyPortfolio.Client/src/components/header/Header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className="border-b border-[#d0d7de] dark:border-[#21262d] bg-[#f6f8fa] dark:bg-[#010409]">
+    <header className="border-b border-[#d0d7de] dark:border-[#21262d] bg-[#f6f8fa] dark:bg-[#0d1117]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <Logo />
