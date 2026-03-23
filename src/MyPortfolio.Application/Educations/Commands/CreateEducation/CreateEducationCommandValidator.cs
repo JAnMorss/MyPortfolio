db6651da0553e128882e8e6f6949cbe8777e675d
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-23 20:15:35 +0800

    feat(education): add CreateEducationCommand validator

---
+++ src/MyPortfolio.Application/Educations/Commands/CreateEducation/CreateEducationCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace MyPortfolio.Application.Educations.Commands.CreateEducation;
+
+internal sealed class CreateEducationCommandValidator
+    : AbstractValidator<CreateEducationCommand>
+{
+    public CreateEducationCommandValidator()
+    {
+        RuleFor(x => x.School)
+            .NotEmpty().WithMessage("School cannot be empty.")
+            .MaximumLength(100).WithMessage("School cannot exceed 100 characters.");
+
+        RuleFor(x => x.Degree)
+            .NotEmpty().WithMessage("Degree cannot be empty.")
+            .MaximumLength(100).WithMessage("Degree cannot exceed 100 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+
+        RuleFor(x => x.StartDate)
+            .NotEmpty().WithMessage("Start date is required.");
+
+        RuleFor(x => x.EndDate)
+            .GreaterThan(x => x.StartDate)
+            .When(x => x.EndDate.HasValue)
+            .WithMessage("End date must be greater than start date.");
+
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.");
+    }
+}
+🎯 What this valid
