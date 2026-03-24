6c0629911f583287eea380d3ea6a823a6666b364
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:22:31 +0800

    feat(application): add UpdateSkillCommandValidator

---
+++ src/MyPortfolio.Application/Skills/Commands/UpdateSkill/UpdateSkillCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Skills.Commands.UpdateSkill;
+
+internal sealed class UpdateSkillCommandValidator
+    : AbstractValidator<UpdateSkillCommand>
+{
+    public UpdateSkillCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Skill ID cannot be empty.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid Skill ID.");
+
+        RuleFor(x => x.SkillName)
+            .NotEmpty().WithMessage("Skill name is required.")
+            .MaximumLength(150).WithMessage("Skill name must not exceed 150 characters.");
+
+        RuleFor(x => x.Level)
+            .NotNull().WithMessage("Skill level is required.")
+            .IsInEnum().WithMessage("Invalid skill level.");
+    }
+}
