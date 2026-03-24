967407b7e82baca1a2d245989e4c80b54872a252
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:21:28 +0800

    feat(application): add CreateExperienceCommandValidator

---
+++ src/MyPortfolio.Application/Experiences/Commands/CreateExperience/CreateExperienceCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Experiences.Commands.CreateExperience;
+
+internal sealed class CreateExperienceCommandValidator
+    : AbstractValidator<CreateExperienceCommand>
+{
+    public CreateExperienceCommandValidator()
+    {
+        RuleFor(x => x.CompanyName)
+            .NotEmpty().WithMessage("Company name is required.")
+            .MaximumLength(200).WithMessage("Company name must not exceed 200 characters.");
+
+        RuleFor(x => x.StartDate)
+            .NotNull().WithMessage("Start date is required.")
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("Start date cannot be in the future.");
+
+        RuleFor(x => x.EndDate)
+            .GreaterThanOrEqualTo(x => x.StartDate!)
+            .When(x => x.StartDate.HasValue && x.EndDate.HasValue)
+            .WithMessage("End date cannot be earlier than start date.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000)
+            .When(x => !string.IsNullOrWhiteSpace(x.Description))
+            .WithMessage("Description must not exceed 1000 characters.");
+
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid User ID.");
+    }
+}
