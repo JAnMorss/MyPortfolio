b5435753ce526282e13a698f56812f669d834ff4
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:21:41 +0800

    feat(application): add DeleteMessageCommandValidator

---
+++ src/MyPortfolio.Application/Messages/Commands/DeleteMessage/DeleteMessageCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Messages.Commands.DeleteMessage;
+
+internal sealed class DeleteMessageCommandValidator
+    : AbstractValidator<DeleteMessageCommand>
+{
+    public DeleteMessageCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Message ID cannot be empty.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid Message ID.");
+    }
+}
