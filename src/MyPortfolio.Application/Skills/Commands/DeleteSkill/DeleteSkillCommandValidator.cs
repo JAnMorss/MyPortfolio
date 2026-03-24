7b0e45945e10adf85a9e78ac21ce0342225391a2
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:22:27 +0800

    feat(application): add DeleteSkillCommandValidator

---
+++ src/MyPortfolio.Application/Skills/Commands/DeleteSkill/DeleteSkillCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Skills.Commands.DeleteSkill;
+
+internal sealed class DeleteSkillCommandValidator
+    : AbstractValidator<DeleteSkillCommand>
+{
+    public DeleteSkillCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Skill ID cannot be empty.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid Skill ID.");
+    }
+}
