a6bd1ac67ee034aa11a86db819f85022b3fe7d97
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:23:16 +0800

    chore(client): update userProfile schema

--- src/MyPortfolio.Client/src/schemas/users/userProfile.schema.ts
+++ src/MyPortfolio.Client/src/schemas/users/userProfile.schema.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 
 export const userProfileSchema = z.object({
   data: z.object({
-    id: z.string().uuid(),
+    id: z.string(),
     fullName: z.string(),
     age: z.number().int().nonnegative(),
     role: z.string(),
-    email: z.string().email(),
-    photo: z.string().url(),
+    email: z.string(),
+    photo: z.string(),
     headLine: z.string(),
     about: z.string(),
     updatedAt: z.string().nullable() 
@@ -15,6 +15,16 @@ export const userProfileSchema = z.object({
   message: z.string()
 });
 
+export const UpdateUserProfileSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  headLine: z.string(),
+  about: z.string(),
+});
+
 export type UserProfileResponse = z.infer<typeof userProfileSchema>;
 
 export type UserProfileData = UserProfileResponse["data"];
+
+export type UpdateUserProfileInput = z.infer<typeof UpdateUserProfileSchema>;
