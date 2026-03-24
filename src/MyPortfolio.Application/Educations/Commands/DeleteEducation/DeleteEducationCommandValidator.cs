5bd0ba616bdca91415f091ada9c9d16472fa2ecc
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:21:20 +0800

    feat(application): add DeleteEducationCommandValidator

---
+++ src/MyPortfolio.Application/Educations/Commands/DeleteEducation/DeleteEducationCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Educations.Commands.DeleteEducation;
+
+internal sealed class DeleteEducationCommandValidator
+    : AbstractValidator<DeleteEducationCommand>
+{
+    public DeleteEducationCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Education ID cannot be empty.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid Education ID.");
+    }
+}
