1874ef93219f042789c52d595e407bd09f0cb068
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:21:14 +0800

    feat(application): add CreateEducationCommandValidator

--- src/MyPortfolio.Application/Educations/Commands/CreateEducation/CreateEducationCommandValidator.cs
+++ src/MyPortfolio.Application/Educations/Commands/CreateEducation/CreateEducationCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿namespace MyPortfolio.Application.Educations.Commands.CreateEducation;
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Educations.Commands.CreateEducation;
 
 internal sealed class CreateEducationCommandValidator
     : AbstractValidator<CreateEducationCommand>
@@ -6,26 +8,29 @@ internal sealed class CreateEducationCommandValidator
     public CreateEducationCommandValidator()
     {
         RuleFor(x => x.School)
-            .NotEmpty().WithMessage("School cannot be empty.")
-            .MaximumLength(100).WithMessage("School cannot exceed 100 characters.");
+            .NotEmpty().WithMessage("School is required.")
+            .MaximumLength(200).WithMessage("School must not exceed 200 characters.");
 
         RuleFor(x => x.Degree)
-            .NotEmpty().WithMessage("Degree cannot be empty.")
-            .MaximumLength(100).WithMessage("Degree cannot exceed 100 characters.");
-
-        RuleFor(x => x.Description)
-            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+            .NotEmpty().WithMessage("Degree is required.")
+            .MaximumLength(200).WithMessage("Degree must not exceed 200 characters.");
 
         RuleFor(x => x.StartDate)
-            .NotEmpty().WithMessage("Start date is required.");
+            .NotNull().WithMessage("Start date is required.");
 
         RuleFor(x => x.EndDate)
-            .GreaterThan(x => x.StartDate)
-            .When(x => x.EndDate.HasValue)
-            .WithMessage("End date must be greater than start date.");
+            .GreaterThanOrEqualTo(x => x.StartDate!)
+            .When(x => x.StartDate.HasValue && x.EndDate.HasValue)
+            .WithMessage("End date cannot be earlier than start date.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000)
+            .When(x => !string.IsNullOrWhiteSpace(x.Description))
+            .WithMessage("Description must not exceed 1000 characters.");
 
         RuleFor(x => x.UserId)
-            .NotEmpty().WithMessage("User ID is required.");
+            .NotEmpty().WithMessage("User ID is required.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid User ID.");
     }
 }
-🎯 What this valid
