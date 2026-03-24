2f4a721e86792330a4dc1095b60a454d8078a33c
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:22:03 +0800

    feat(application): add DeleteProjectCommandValidator

---
+++ src/MyPortfolio.Application/Projects/Commands/DeleteProject/DeleteProjectCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Projects.Commands.DeleteProject;
+
+internal sealed class DeleteProjectCommandValidator
+    : AbstractValidator<DeleteProjectCommand>
+{
+    public DeleteProjectCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Project ID cannot be empty.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid Project ID.");
+    }
+}
