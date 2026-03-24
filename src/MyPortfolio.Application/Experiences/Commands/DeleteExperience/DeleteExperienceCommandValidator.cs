6d991f2d8b0ea6146c8b179a56cc9a22b752b9b0
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-24 20:21:31 +0800

    feat(application): add DeleteExperienceCommandValidator

---
+++ src/MyPortfolio.Application/Experiences/Commands/DeleteExperience/DeleteExperienceCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MyPortfolio.Application.Experiences.Commands.DeleteExperience;
+
+internal sealed class DeleteExperienceCommandValidator
+    : AbstractValidator<DeleteExperienceCommand>
+{
+    public DeleteExperienceCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Experience ID cannot be empty.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("Invalid Experience ID.");
+    }
+}
