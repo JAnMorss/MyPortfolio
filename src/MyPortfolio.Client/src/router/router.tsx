3f73d325b0f85730bad6e851e8d1cf7c11466335
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-31 18:56:54 +0800

    Update routing configuration

--- src/MyPortfolio.Client/src/router/router.tsx
+++ src/MyPortfolio.Client/src/router/router.tsx
@@ -1,18 +1,24 @@
-import EducationsPage from "@/pages/EducationsPage";
-import ExperiencesPage from "@/pages/ExperiencesPage";
-import MessagesPage from "@/pages/MessagesPage";
-import OverviewPage from "@/pages/OverviewPage";
-import ProjectsPage from "@/pages/ProjectsPage";
-import SkillsPage from "@/pages/SkillsPage";
 import { createBrowserRouter } from "react-router-dom";
-
+import AppLayout from "../AppLayout";
+import OverviewPage from "@/pages/OverviewPage";
+import EducationsPage from "@/pages/EducationsPage";
+import SkillsPage from "@/pages/SkillsPage";
+import ExperiencesPage from "@/pages/ExperiencesPage";
+import ProjectsPage from "@/pages/ProjectsPage";
+import MessagesPage from "@/pages/MessagesPage";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <OverviewPage /> },
-  { path: "/overview", element: <OverviewPage /> },
-  { path: "/education", element: <EducationsPage /> },
-  { path: "/skills", element: <SkillsPage /> },
-  { path: "/experience", element: <ExperiencesPage /> },
-  { path: "/projects", element: <ProjectsPage /> },
-  { path: "/messages", element: <MessagesPage /> },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { index: true, element: <OverviewPage /> },
+      { path: "overview", element: <OverviewPage /> },
+      { path: "education", element: <EducationsPage /> },
+      { path: "skills", element: <SkillsPage /> },
+      { path: "experience", element: <ExperiencesPage /> },
+      { path: "projects", element: <ProjectsPage /> },
+      { path: "messages", element: <MessagesPage /> },
+    ],
+  },
 ]);
