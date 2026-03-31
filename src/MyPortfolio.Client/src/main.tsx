10992a5881f64ec0d3d81c3055ed4aebec391723
JAnMorss <johnanthonymorales124@gmail.com>
2026-03-31 18:56:46 +0800

    Update main entry point

--- src/MyPortfolio.Client/src/main.tsx
+++ src/MyPortfolio.Client/src/main.tsx
@@ -5,13 +5,11 @@ import App from './App.tsx'
 import ThemeProvider from './components/ThemeProvider.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './hooks/useAuth.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
             <AuthProvider>
@@ -19,6 +17,5 @@ createRoot(document.getElementById('root')!).render(
             </AuthProvider>
         </ThemeProvider>
       </QueryClientProvider>
-    </BrowserRouter>
   </StrictMode>,
 )
