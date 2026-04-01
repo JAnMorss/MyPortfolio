958ef2ad0222e05cf7b11623490c823188e2cc09
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-01 18:32:12 +0800

    Add/update message API connector

---
+++ src/MyPortfolio.Client/src/api.connector/message/message.api.connector.ts
@@ -0,0 +1,78 @@
+import {
+  messageListSchema,
+  messageItemSchema,
+  MessageInputSchema,
+  type MessageListData,
+  type MessageItem,
+  type MessageInput,
+} from "@/schemas/message/message.schema";
+
+import axios, {
+  type AxiosInstance,
+  type InternalAxiosRequestConfig,
+  AxiosHeaders,
+  type AxiosResponse,
+} from "axios";
+
+const BASE_URL = "http://localhost:8026/api/v1";
+
+const api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      if (!config.headers) {
+        config.headers = new AxiosHeaders();
+      }
+      config.headers.set("Authorization", `Bearer ${token}`);
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error) => Promise.reject(error)
+);
+
+export const messageApiConnector = {
+  sendMessage: async (data: MessageInput): Promise<void> => {
+    const validData = MessageInputSchema.parse(data);
+
+    await api.post("/message", validData);
+  },
+  
+  getMessages: async (): Promise<MessageListData> => {
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("Unauthorized");
+
+    const response = await api.get("/message");
+
+    return messageListSchema.parse(response.data).data;
+  },
+
+  getMessageById: async (id: string): Promise<MessageItem> => {
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("Unauthorized");
+
+    const response = await api.get(`/message/${id}`);
+
+    return messageItemSchema.parse(response.data.data);
+  },
+
+  deleteMessage: async (id: string): Promise<void> => {
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("Unauthorized");
+
+    await api.delete(`/message/${id}`);
+  },
+};
