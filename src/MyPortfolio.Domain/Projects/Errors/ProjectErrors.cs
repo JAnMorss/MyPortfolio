ea9e4dd7332e2580c65c5d3a8155c977e012faa8
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-06 18:41:08 +0800

    Fix ProjectErrors.cs error definitions

--- src/MyPortfolio.Domain/Projects/Errors/ProjectErrors.cs
+++ src/MyPortfolio.Domain/Projects/Errors/ProjectErrors.cs
@@ -7,4 +7,12 @@ public static class ProjectErrors
     public static readonly Error NotFound = new(
         "Project.NotFound",
         "The Project with the specified identifier was not found.");
+
+    public static readonly Error MediaNotFound = new(
+        "Project.MediaNotFound",
+        "The media associated with the Project was not found.");
+
+    public static readonly Error InvalidMedia = new(
+        "Project.InvalidMedia",
+        "The media URL associated with the Project is invalid.");
 }
