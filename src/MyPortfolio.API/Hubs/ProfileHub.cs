807e01c6e2e3046c2629476a07a4f366c8ef6818
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-07 17:12:43 +0800

    feat: add hubs

---
+++ src/MyPortfolio.API/Hubs/ProfileHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MyPortfolio.API.Hubs;
+
+public class ProfileHub : Hub
+{
+    public async Task JoinProfileGroup(string userId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+    }
+}
---
+++ src/MyPortfolio.API/Hubs/UserProfileViewedDomainEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using MyPortfolio.Domain.Users.Events;
+
+namespace MyPortfolio.API.Hubs;
+
+public sealed class UserProfileViewedDomainEventHandler
+    : INotificationHandler<UserProfileViewedDomainEvent>
+{
+    private readonly IHubContext<ProfileHub> _hubContext;
+    public UserProfileViewedDomainEventHandler(IHubContext<ProfileHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Handle(
+        UserProfileViewedDomainEvent notification,
+        CancellationToken cancellationToken)
+    {
+        await _hubContext.Clients
+            .Group(notification.UserId.ToString())
+            .SendAsync("ProfileViewed", notification.TotalViews, cancellationToken);
+    }
+}
