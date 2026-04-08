8fc4e7d7da74047e5af8a21cfc02eb12a4618a39
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-08 20:22:04 +0800

    Add PortfolioHub.cs

---
+++ src/MyPortfolio.API/Hubs/PortfolioHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MyPortfolio.Application.Abstractions.Realtime;
+
+namespace MyPortfolio.API.Hubs;
+
+public class PortfolioHub : Hub
+{
+    private readonly IProfileViewService _viewService;
+
+    public PortfolioHub(IProfileViewService viewService)
+    {
+        _viewService = viewService;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var count = _viewService.Increment();
+
+        await Clients.All.SendAsync("ReceiveViewCount", count);
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        // var count = _viewService.Decrement();
+        // await Clients.All.SendAsync("ReceiveViewCount", count);
+
+        await base.OnDisconnectedAsync(exception);
+    }
+}
