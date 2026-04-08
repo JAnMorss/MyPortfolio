6cb4345b5ed188bf4119df95a902a1bf13b19142
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-08 20:21:01 +0800

    Update vite.config.ts

--- src/MyPortfolio.Client/vite.config.ts
+++ src/MyPortfolio.Client/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
