622b7eef68bc2c20990ab1ab51f2f3de2ce97aad
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-08 20:22:11 +0800

    Add Realtime infrastructure

---
+++ src/MyPortfolio.Infrastructure/Realtime/ProfileViewService.cs
@@ -0,0 +1,18 @@
+﻿using MyPortfolio.Application.Abstractions.Realtime;
+
+namespace MyPortfolio.Infrastructure.Realtime;
+
+public class ProfileViewService : IProfileViewService
+{
+    private int _count = 0;
+    public int GetCount()
+    {
+        return _count;
+    }
+
+    public int Increment()
+    {
+        _count++;
+        return _count;
+    }
+}
