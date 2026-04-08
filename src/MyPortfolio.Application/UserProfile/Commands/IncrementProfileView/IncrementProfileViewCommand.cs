6837e56b8930b5159276805da9fd07efdee1f770
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-08 20:23:10 +0800

    Remove IncrementProfileViewCommand.cs

--- src/MyPortfolio.Application/UserProfile/Commands/IncrementProfileView/IncrementProfileViewCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using MyPortfolio.SharedKernel.Mediators.Command;
-
-namespace MyPortfolio.Application.UserProfile.Commands.IncrementProfileView;
-
-public sealed record IncrementProfileViewCommand(Guid UserId) : ICommand<int>;
