3848e9ee6d42c722186c9403554b75d7a32a5652
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-08 20:23:13 +0800

    Remove UserProfileViewedDomainEvent.cs

--- src/MyPortfolio.Domain/Users/Events/UserProfileViewedDomainEvent.cs
+++
@@ -1,5 +0,0 @@
-﻿using MyPortfolio.SharedKernel.Domain;
-
-namespace MyPortfolio.Domain.Users.Events;
-
-public sealed record UserProfileViewedDomainEvent(Guid UserId, int TotalViews) : IDomainEvent;
