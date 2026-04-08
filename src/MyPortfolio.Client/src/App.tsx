72153ec58cfe6e51a725dbef430453fd17445139
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-08 20:20:54 +0800

    Update App.tsx

--- src/MyPortfolio.Client/src/App.tsx
+++ src/MyPortfolio.Client/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
-import { AuthProvider } from "./hooks/useAuth";
+import { AuthProvider } from "./hooks/auth/useAuth.tsx";
 
 function App() {
   return (
