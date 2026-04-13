e6cd778757ff41e2b5b85b8a9a61cb831868413c
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-13 20:35:09 +0800

    feat: update editable field component

--- src/MyPortfolio.Client/src/components/sidebarProfile/EditableField.tsx
+++ src/MyPortfolio.Client/src/components/sidebarProfile/EditableField.tsx
@@ -1,21 +1,24 @@
 "use client";
 
+import clsx from "clsx";
+
 type EditableFieldProps = {
   value: string;
   isEditMode: boolean;
   onChange: (val: string) => void;
   placeholder?: string;
+  className?: string;
 };
 
-export default function EditableField({ value, isEditMode, onChange, placeholder }: EditableFieldProps) {
+export default function EditableField({ value, isEditMode, onChange, placeholder, className }: EditableFieldProps) {
   return isEditMode ? (
     <input
       value={value}
       onChange={(e) => onChange(e.target.value)}
       placeholder={placeholder}
-      className="border rounded px-2 py-1 w-full"
+      className={clsx("border rounded px-2 py-1 w-full", className)}
     />
   ) : (
-    <p>{value}</p>
+    <p className={className}>{value}</p>
   );
 }
