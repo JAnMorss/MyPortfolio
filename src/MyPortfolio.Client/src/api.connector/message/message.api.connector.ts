694519440baacfe2eea6913f3a5785c911ccb1f5
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-13 20:35:04 +0800

    feat: update message API connector

--- src/MyPortfolio.Client/src/api.connector/message/message.api.connector.ts
+++ src/MyPortfolio.Client/src/api.connector/message/message.api.connector.ts
@@ -51,12 +51,13 @@ export const messageApiConnector = {
     await api.post("/message", validData);
   },
   
-  getMessages: async (page: number = 1, pageSize: number = 10, search?: string): Promise<MessageListData> => {
+  getMessages: async (page: number = 1, pageSize: number = 10, search?: string, sortBy?: string): Promise<MessageListData> => {
     const token = localStorage.getItem("token");
     if (!token) throw new Error("Unauthorized");
 
     const params: any = { page, pageSize };
     if (search) params.search = search;
+    if (sortBy) params.sortBy = sortBy;
     const response = await api.get("/message", { params });
 
     return messageListSchema.parse(response.data).data;
