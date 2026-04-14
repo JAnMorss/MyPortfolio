3075fe2007114666aa35b14660000cb34247faf0
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-14 16:44:26 +0800

    Refactor AppLayout structure and layout adjustments

--- src/MyPortfolio.Client/src/AppLayout.tsx
+++ src/MyPortfolio.Client/src/AppLayout.tsx
@@ -8,9 +8,9 @@ export default function AppLayout() {
     <div className="min-h-screen bg-white text-black dark:bg-[#212830] dark:text-white">
       <Header />
 
-      <div className="max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col lg:flex-row gap-6">  
-          <div className="lg:w-[296px]">
+          <div className="lg:w-74">
             <SidebarProfile />
           </div>
 
