cd0920e61452a71f4b044e527e3af8515eeae371
JAnMorss <johnanthonymorales124@gmail.com>
2026-04-27 18:19:06 +0800

    feat: update message API connector

--- src/MyPortfolio.Client/src/api.connector/message/message.api.connector.ts
+++ src/MyPortfolio.Client/src/api.connector/message/message.api.connector.ts
@@ -7,42 +7,9 @@ import {
   type MessageInput,
 } from "@/schemas/message/message.schema";
 
-import axios, {
-  type AxiosInstance,
-  type InternalAxiosRequestConfig,
-  AxiosHeaders,
-  type AxiosResponse,
-} from "axios";
+import { createApiClient } from "@/api.connector/axios.client";
 
-const BASE_URL = "http://localhost:8026/api/v1";
-
-const api: AxiosInstance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-api.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      if (!config.headers) {
-        config.headers = new AxiosHeaders();
-      }
-      config.headers.set("Authorization", `Bearer ${token}`);
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (response: AxiosResponse) => response,
-  (error) => Promise.reject(error)
-);
+const api = createApiClient();
 
 export const messageApiConnector = {
   sendMessage: async (data: MessageInput): Promise<void> => {
